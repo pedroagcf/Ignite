7f2ca077dd6e4e227562f0ffcd33e8454bd65078
Pedro Augusto <26pedrocastro@gmail.com>
2021-07-29 18:39:15 -0300

    chapter 2 - Contextos e hooks

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
+import { TransactionsProvider } from './hooks/useTransactions';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
@@ -19,7 +20,7 @@ export function App() {
   }
 
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
       <NewTransactionModal 
@@ -27,6 +28,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}  
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
